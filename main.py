5ef7825d8417d05ad8be74f2bf61d34637219763
nate-4000 <nathanieltebo8@gmail.com>
2023-03-11 20:16:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# whoopies i forgor the code
+import os
+
+inputfile = None
+while inputfile is None or not os.path.exists(inputfile):
+    inputfile = input("Enter a valid file path: ")
+
+byte_list = []
+
+with open(inputfile, "rb") as f:
+    byte = f.read(1)
+    while byte:
+        byte_list.append(ord(byte))
+        byte = f.read(1)
+
+print(byte_list)
