b940d5f72a95e2358a999cbab7e1a522885f9cb4
nate-4000 <nathanieltebo8@gmail.com>
2023-03-11 20:21:26 -0800

    ty chatgpt :+1:

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ while inputfile is None or not os.path.exists(inputfile):
     inputfile = input("Enter a valid file path: ")
 
 byte_list = []
+byte_pairs = []
 
 with open(inputfile, "rb") as f:
     byte = f.read(1)
     while byte:
         byte_list.append(ord(byte))
         byte = f.read(1)
+if len(byte_list) % 2 != 0:
+    byte_list.pop()  # whoopsies, overflow :(
+for i in range(0, len(byte_list), 2):
+    byte_pairs.append((byte_list[i], byte_list[i+1]))
 
-print(byte_list)
+
+print(byte_pairs)
